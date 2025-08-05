8f0fd52dc9260dcfb8a330a5a96ee15e7fdb6708
Hamza <testdata165@gmail.com>
2025-08-05 13:50:41 +0500

    Update and rename __manifest__.p to __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    'name': 'l10n_mx_efos',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'summary': 'A module to demonstrate a simple test in Odoo',
+    'description': """
+        This is a test module to demonstrate how to create a simple test in Odoo.
+    """,
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [    ],
+    'installable': True,
+    'application': False,
+}
